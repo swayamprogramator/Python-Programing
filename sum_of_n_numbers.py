cd37947924415ebc6b4b3feb4c151272db6294f0
Harsh Sharma <student3.ai@csvtu.ac.in>
2024-08-10 19:49:51 +0530

    Create sum_of_n_numbers.py

---
+++ sum_of_n_numbers.py
@@ -0,0 +1,11 @@
+num = int(input('Enter a number: '))
+
+# find sum of natural number
+sum = 0
+x = 1
+while x <= num:
+ sum += x 
+ x += 1
+
+# display result
+print('The sum of natural number =', sum)
