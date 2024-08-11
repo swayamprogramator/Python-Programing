eb90739bc7d388dc2d3cdad027b2b437804e4914
Harsh Sharma <student3.ai@csvtu.ac.in>
2024-08-11 07:26:02 +0530

    Create cel_to_farenheit.py

---
+++ cel_to_farenheit.py
@@ -0,0 +1,6 @@
+cel = float(input('Enter temperature in Celsius: '))
+
+fahr = (cel * 1.8) + 32
+
+print('%0.1f degrees Celsius is equivalent to %0.1f 
+                       degrees Fahrenheit' %(cel, fahr))
