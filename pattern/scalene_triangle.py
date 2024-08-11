b409037cd8af652aaf1c8149545e6a9c1d1b4d8d
Harsh Sharma <student3.ai@csvtu.ac.in>
2024-08-11 20:10:42 +0530

    Create scalene_triangle.py

---
+++ pattern/scalene_triangle.py
@@ -0,0 +1,15 @@
+def pattern(n):
+   # number of spaces
+   a = n - 1
+   for i in range(n):
+      for j in range(a):
+         print(end=" ")
+         
+      # decrementing a after each loop
+      a = a - 1
+      for j in range(i+1):
+         print("* ",end="  ")
+      print("\r")
+
+n = int(input('Enter the number of rows: '))
+pattern(n)
