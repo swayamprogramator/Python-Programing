2eba5762f76c07f23a19535134fca22040657042
Harsh Sharma <student3.ai@csvtu.ac.in>
2024-08-12 07:25:34 +0530

    Create matrix_multiplication_using_numpy_dot.py

---
+++ NumPy/matrix_multiplication_using_numpy_dot.py
@@ -0,0 +1,17 @@
+import numpy
+
+# take inputs
+m1 = [[1, 7],
+      [4, 5]]
+
+m2 = [[5, 3],
+      [4, 2]]
+
+res = [[0, 0],
+       [0, 0]]
+
+# multiply matrix
+print("Matrix Multiplication: ")
+result = numpy.dot(m1, m2)
+for row in result:
+   print(row)
