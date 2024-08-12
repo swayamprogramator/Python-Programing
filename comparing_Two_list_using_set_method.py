a4e7b20099418c7c543e3e6e911909f3624e6650
Harsh Sharma <student3.ai@csvtu.ac.in>
2024-08-12 07:04:14 +0530

    Create comparing_Two_list_using_set_method.py

---
+++ comparing_Two_list_using_set_method.py
@@ -0,0 +1,10 @@
+list1 = [1, 2, 3]  
+list2 = [1, 2, 3]  
+  
+a = set(list1)  
+b = set(list2)  
+  
+if a == b:  
+   print("The list1 and list2 are equal")  
+else:  
+   print("The list1 and list2 are not equal")
