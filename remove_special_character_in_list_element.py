618f8600f0ce1feea5586d7c31d8cfab47f01c9d
Harsh Sharma <student3.ai@csvtu.ac.in>
2024-08-12 06:45:14 +0530

    Update remove_special_character_in_list_element.py

--- remove_special_character_in_list_element.py
+++ remove_special_character_in_list_element.py
@@ -1,10 +1,8 @@
 my_list = ['@know*', 'pr#ogra!m^', '(py_th@on_3}']
 
-# initializing special characters
 special_char = '@_!#$%^&*()<>?/\|}{~:;.[]'
  
 # using join() + generator to remove special characters
 out_list = [''.join(x for x in string if not x in special_char) for string in my_list]
  
-# print list without special characters
 print('List after removal of special characters:', out_list)
